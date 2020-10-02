5061313327ec824b33ededf6846464d392d396af
Michael Wilt <michaeljwilt@outlook.com>
2020-10-02 00:29:25 -0400

    Sensitive Information Delete

--- email_verify/urls.py
+++ email_verify/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.home),
+    path('verify/', views.verify),
+    path('send/', views.send),
 ]
--- email_verify/views.py
+++ email_verify/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello World")
+def home(request):
+    return render(request, 'email_verify/index.html')
 
-# Create your views here.
+
+def verify(request):
+    return render(request, 'email_verify/verify.html')
+
+
+def send(request):
+    return render(request, 'email_verify/send.html')
